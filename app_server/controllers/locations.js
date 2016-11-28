66e9c8ed94b6d7e5aa8e88a8ea7f5bb508f36611
viniciobb <viniciobb@gmail.com>
2016-11-28 20:07:31 -0200

    alteração titulo

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -2,7 +2,12 @@
 /* GET 'home' page using index extending layout*/
 
 module.exports.homeList = function(req, res, next) {
-  res.render('location-list', { title: 'Home' });
+  res.render('location-list', { title: 'Loc8r - find a place to work with wifi',
+                                pageHeader : { 
+                                  title : "Loc8r",
+                                  strapline : 'Find place sto work with wifi near you !'
+                                } 
+                              });
 };
 
 /* GET 'Location info' page using index extending layout*/
